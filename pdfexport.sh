01947e0114f1303cb09d7de0307f4a826365340e
Michele Beltrame <mb@cattlegrid.info>
2011-11-03 09:44:27 +0100

    Fix shell script

--- pdfexport.sh
+++ pdfexport.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+
 wkhtmltopdf --margin-top 0 --margin-bottom 0 --margin-left 0 --margin-right 0 --page-width 199.5 --page-height 150  --print-media-type --outline-depth 0 presentazion.html presentazion.pdf
